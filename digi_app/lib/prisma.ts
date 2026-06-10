37bcae6363ca52c38a991f50a54c425e7e5b4c29
Alvin Ababil <neoryumasil@gmail.com>
2026-06-10 09:51:22 +0700

    ADD ORM Database

---
+++ digi_app/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg'; 
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+function makePrisma() {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+  const adapter = new PrismaPg(pool);
+  
+  return new PrismaClient({ 
+    adapter, 
+    log: ['query', 'error', 'warn'] 
+  });
+}
+
+export const prisma = globalForPrisma.prisma || makePrisma();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
